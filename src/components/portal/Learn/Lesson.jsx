87ed0f911afb3f271ce67dafca29eaa4734c233f
JUSTINE IMASIKU <justineimasiku21@gmail.com>
2024-05-10 16:26:19 +0000

    update the ui

--- src/components/portal/Learn/Lesson.jsx
+++ src/components/portal/Learn/Lesson.jsx
@@ -6,23 +6,28 @@ import rec3 from "../../../assets/audios/Piggerymanagement3.m4a";
 import rec4 from "../../../assets/audios/Piggerymanagement4.m4a";
 import rec5 from "../../../assets/audios/Piggerymanagement5.m4a";
 import rec6 from "../../../assets/audios/Piggerymanagement6.m4a";
+import { NavLink } from "react-router-dom";
 
-const Lessonn = ({ lesson, lessons, completeModule }) => {
-   if (lesson === 1) {
-  return (
-    <div className="lesson">
-      <div className="lesn">
-        <h3>Piggery Management Module {lesson} </h3>
-        <h2>{lessons[0].title}</h2>
+const Lessonn = ({ lesson, lessons, completeModule, redoModule }) => {
+  if (lesson === 1) {
+    return (
+      <div className="lesson">
+        <div className="lesn">
+          <h3>Piggery Management Module {lesson} </h3>
+          <h2>{lessons[0].title}</h2>
+        </div>
+        <audio controls>
+          <source src={rec1} type="audio/ogg" />
+          <source src={rec1} type="audio/mpeg" />
+          Your browser does not support the audio tag.
+        </audio>
+        <div className="btnss">
+          <button className="prev" onClick={() => redoModule()}>Previous Lesson</button>
+          <button className="next" onClick={() => completeModule()}>Next Lesson</button>
+        </div>
       </div>
-      <audio controls>
-        <source src={lessons[0].recording} type="audio/ogg" />
-        <source src={lessons[0].recording} type="audio/mpeg" />
-        Your browser does not support the audio tag.
-      </audio>
-      <button onClick={() => completeModule()}>Next Lesson</button>
-    </div>
-  );} else if (lesson === 2 ) {
+    );
+  } else if (lesson === 2) {
     return (
       <div className="lesson">
         <div className="lesn">
@@ -30,14 +35,17 @@ const Lessonn = ({ lesson, lessons, completeModule }) => {
           <h2>{lessons[1].title}</h2>
         </div>
         <audio controls>
-          <source src={lessons[1].recording} type="audio/ogg" />
-          <source src={lessons[1].recording} type="audio/mpeg" />
+          <source src={rec1} type="audio/ogg" />
+          <source src={rec2} type="audio/mpeg" />
           Your browser does not support the audio tag.
         </audio>
-        <button onClick={() => completeModule()}>Next Lesson</button>
+        <div className="btnss">
+        <button className="prev" onClick={() => redoModule()}>Previous Lesson</button>
+        <button className="next" onClick={() => completeModule()}>Next Lesson</button>
+      </div>
       </div>
     );
-  } else if (lesson === 3 ) {
+  } else if (lesson === 3) {
     return (
       <div className="lesson">
         <div className="lesn">
@@ -45,11 +53,14 @@ const Lessonn = ({ lesson, lessons, completeModule }) => {
           <h2>{lessons[2].title}</h2>
         </div>
         <audio controls>
-          <source src={lessons[2].recording} type="audio/ogg" />
-          <source src={lessons[2].recording} type="audio/mpeg" />
+          <source src={rec3} type="audio/ogg" />
+          <source src={rec3} type="audio/mpeg" />
           Your browser does not support the audio tag.
         </audio>
-        <button onClick={() => completeModule()}>Next Lesson</button>
+        <div className="btnss">
+          <button className="prev" onClick={() => redoModule()}>Previous Lesson</button>
+          <button className="next" onClick={() => completeModule()}>Next Lesson</button>
+        </div>
       </div>
     );
   } else if (lesson === 4) {
@@ -60,14 +71,17 @@ const Lessonn = ({ lesson, lessons, completeModule }) => {
           <h2>{lessons[3].title}</h2>
         </div>
         <audio controls>
-          <source src={lessons[3].recording} type="audio/ogg" />
-          <source src={lessons[3].recording} type="audio/mpeg" />
+          <source src={rec4} type="audio/ogg" />
+          <source src={rec4} type="audio/mpeg" />
           Your browser does not support the audio tag.
         </audio>
-        <button onClick={() => completeModule()}>Next Lesson</button>
+        <div className="btnss">
+          <button className="prev" onClick={() => redoModule()}>Previous Lesson</button>
+          <button className="next" onClick={() => completeModule()}>Next Lesson</button>
+        </div>
       </div>
     );
-  } else if (lesson === 5 ) {
+  } else if (lesson === 5) {
     return (
       <div className="lesson">
         <div className="lesn">
@@ -75,14 +89,17 @@ const Lessonn = ({ lesson, lessons, completeModule }) => {
           <h2>{lessons[4].title}</h2>
         </div>
         <audio controls>
-          <source src={lessons[4].recording} type="audio/ogg" />
-          <source src={lessons[4].recording} type="audio/mpeg" />
+          <source src={rec5} type="audio/ogg" />
+          <source src={rec5} type="audio/mpeg" />
           Your browser does not support the audio tag.
         </audio>
-        <button onClick={() => completeModule()}>Next Lesson</button>
+        <div className="btnss">
+          <button className="prev" onClick={() => redoModule()}>Previous Lesson</button>
+          <button className="next" onClick={() => completeModule()}>Next Lesson</button>
+        </div>
       </div>
     );
-  } else if (lesson === 6 ) {
+  } else if (lesson === 6) {
     return (
       <div className="lesson">
         <div className="lesn">
@@ -90,11 +107,18 @@ const Lessonn = ({ lesson, lessons, completeModule }) => {
           <h2>{lessons[5].title}</h2>
         </div>
         <audio controls>
-          <source src={lessons[5].recording} type="audio/ogg" />
-          <source src={lessons[5].recording} type="audio/mpeg" />
+          <source src={rec6} type="audio/ogg" />
+          <source src={rec6} type="audio/mpeg" />
           Your browser does not support the audio tag.
         </audio>
-        <button onClick={() => completeModule()}>Next Lesson</button>
+
+
+        <div className="btnss">
+          <button className="prev" onClick={() => redoModule()}>Previous Lesson</button>
+          <NavLink to="/dashboard" className="next">Done</NavLink>
+        </div>
+        
+       
       </div>
     );
   }
@@ -130,11 +154,20 @@ const Lesson = () => {
     },
   ];
 
+  const redoModule = () => {
+    setLesson(lesson - 1);
+  };
+
   const completeModule = () => {
     setLesson(lesson + 1);
   };
   return (
-    <Lessonn lesson={lesson} lessons={lessons} completeModule={completeModule} />
+    <Lessonn
+      lesson={lesson}
+      lessons={lessons}
+      completeModule={completeModule}
+      redoModule={redoModule}
+    />
   );
 };
 
