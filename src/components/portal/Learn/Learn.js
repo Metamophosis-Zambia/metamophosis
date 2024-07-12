43db9e0345a6d3d544137c0164120e6a8d89ca73
E-Chisulo <enochen.za@gmail.com>
2024-07-12 15:22:56 +0200

    dashboard looks, enhanced learning cards

--- src/components/portal/Learn/Learn.js
+++ src/components/portal/Learn/Learn.js
@@ -13,42 +13,108 @@ const Learn = () => (
 		{/* <MinNav /> */}
 		<div className="portal-page-content">
 			<ul className="lec">
-			<li>
+				
+
+			  <li>
 					<Link to="/dashboard/learn/animals" activestyle="true">
 						<img src={cattle} alt="crop" />
-						Livestock Management
+						
 					</Link>
-				</li>
-				<li>
-					<Link to="/dashboard/learn/crops" activestyle="true">
+					<div className='learnCard'>
+					  <ul>
+						<h3>
+					    	Livestock Management
+					  	</h3>
+
+						<p>$5.00</p>
+					  </ul>
+
+					  <li className='courseDetails'>
+						<p>
+							Practices and techniques used to raise and care for animals in a sustainable and efficient manner
+						</p>
+					  </li>
+					</div>
+			   </li>
+
+			   <li>
+			   <Link to="/dashboard/learn/crops" activestyle="true">
 						<img src={crop} alt="crop" />
-						<p>Crop Management</p>
 					</Link>
-				</li>
-				
-				<li>
-					<Link to="/dashboard/cropman" activestyle="true">
+					<div className='learnCard'>
+					  <ul>
+						<h3>
+						Crop Management
+					  	</h3>
+
+						<p>$5.00</p>
+					  </ul>
+
+					  <li className='courseDetails'>
+						<p>
+							Practices and techniques used to insure higher crop hields 
+						</p>
+					  </li>
+					</div>
+			   </li>
+
+			   <li>
+			    <Link to="/dashboard/cropman" activestyle="true">
 						<img src={sus} alt="crop" />
+					</Link>
+					<div className='learnCard'>
+					  <ul>
+						<h3>
 						Sustainable Agriculture
-					</Link>
-				</li>
-				<li>
-					<Link to="/dashboard/satimg" activestyle="true">
+					  	</h3>
+
+						<p>$5.00</p>
+					  </ul>
+
+					  <li className='courseDetails'>
+						<p>
+							Eco freindly and preservative farming that aims to produce more crop in small land spaces and reusability
+						</p>
+					  </li>
+					</div>
+			   </li>
+				
+			   <li>
+			   <Link to="/dashboard/satimg" activestyle="true">
 						<img src={csa} alt="crop" />
-						Climate Smart Agriculture
 					</Link>
-				</li>
+					<div className='learnCard'>
+					  <ul>
+						<h3>
+						Climate Smart Agriculture
+					  	</h3>
+
+						<p>$5.00</p>
+					  </ul>
+
+					  <li className='courseDetails'>
+						<p>
+							Laveraging technology to make agriculture more efficinet and sustainable. 
+						</p>
+					  </li>
+					</div>
+			   </li>
+
+
+
+
+
 				{/* <li>
 					<Link to="/dashboard/vitsup" activestyle="true">
 						<img src={ta} alt="crop" />
-						Tech in Agriculture
 					</Link>
+					<p>Tech in Agriculture</p>
 				</li>
 				<li>
 					<Link to="/dashboard/marketplace" activestyle="true">
 						<img src={fn} alt="crop" />
-						Finest Tips
 					</Link>
+					<p>Finest Tips</p>
 				</li> */}
 			</ul>
 		</div>
