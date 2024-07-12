fd1d13b3da6b9bbc0b119e869ba2927d5caf3dfe
E-Chisulo <enochen.za@gmail.com>
2024-07-12 16:30:47 +0200

    enhanced animals page as well and refactored dashboard and animal pages to render data from data sets for easier scaling

--- src/components/portal/Learn/Learn.js
+++ src/components/portal/Learn/Learn.js
@@ -5,120 +5,68 @@ import crop from '../images/onionforsale.jpeg';
 import cattle from '../images/Cattle.jpeg';
 import csa from '../images/farm.jpeg';
 import sus from '../images/MaketsF.jpg';
-import fn from '../images/ta.png';
-import ta from '../images/full insight.png';
+// import fn from '../images/ta.png';
+// import ta from '../images/full_insight.png';
+
+const courses = [
+  {
+    path: "/dashboard/learn/animals",
+    imgSrc: cattle,
+    imgAlt: "livestock",
+    title: "Livestock Management",
+    price: "$5.00",
+    description: "Practices and techniques used to raise and care for animals in a sustainable and efficient manner"
+  },
+  {
+    path: "/dashboard/learn/crops",
+    imgSrc: crop,
+    imgAlt: "crop",
+    title: "Crop Management",
+    price: "$5.00",
+    description: "Practices and techniques used to ensure higher crop yields"
+  },
+  {
+    path: "/dashboard/cropman",
+    imgSrc: sus,
+    imgAlt: "sustainable agriculture",
+    title: "Sustainable Agriculture",
+    price: "$5.00",
+    description: "Eco-friendly and preservative farming that aims to produce more crops in small land spaces and reusability"
+  },
+  {
+    path: "/dashboard/satimg",
+    imgSrc: csa,
+    imgAlt: "climate smart agriculture",
+    title: "Climate Smart Agriculture",
+    price: "$5.00",
+    description: "Leveraging technology to make agriculture more efficient and sustainable."
+  }
+];
 
 const Learn = () => (
-	<div className="portal-content">
-		{/* <MinNav /> */}
-		<div className="portal-page-content">
-			<ul className="lec">
-				
-
-			  <li>
-					<Link to="/dashboard/learn/animals" activestyle="true">
-						<img src={cattle} alt="crop" />
-						
-					</Link>
-					<div className='learnCard'>
-					  <ul>
-						<h3>
-					    	Livestock Management
-					  	</h3>
-
-						<p>$5.00</p>
-					  </ul>
-
-					  <li className='courseDetails'>
-						<p>
-							Practices and techniques used to raise and care for animals in a sustainable and efficient manner
-						</p>
-					  </li>
-					</div>
-			   </li>
-
-			   <li>
-			   <Link to="/dashboard/learn/crops" activestyle="true">
-						<img src={crop} alt="crop" />
-					</Link>
-					<div className='learnCard'>
-					  <ul>
-						<h3>
-						Crop Management
-					  	</h3>
-
-						<p>$5.00</p>
-					  </ul>
-
-					  <li className='courseDetails'>
-						<p>
-							Practices and techniques used to insure higher crop hields 
-						</p>
-					  </li>
-					</div>
-			   </li>
-
-			   <li>
-			    <Link to="/dashboard/cropman" activestyle="true">
-						<img src={sus} alt="crop" />
-					</Link>
-					<div className='learnCard'>
-					  <ul>
-						<h3>
-						Sustainable Agriculture
-					  	</h3>
-
-						<p>$5.00</p>
-					  </ul>
-
-					  <li className='courseDetails'>
-						<p>
-							Eco freindly and preservative farming that aims to produce more crop in small land spaces and reusability
-						</p>
-					  </li>
-					</div>
-			   </li>
-				
-			   <li>
-			   <Link to="/dashboard/satimg" activestyle="true">
-						<img src={csa} alt="crop" />
-					</Link>
-					<div className='learnCard'>
-					  <ul>
-						<h3>
-						Climate Smart Agriculture
-					  	</h3>
-
-						<p>$5.00</p>
-					  </ul>
-
-					  <li className='courseDetails'>
-						<p>
-							Laveraging technology to make agriculture more efficinet and sustainable. 
-						</p>
-					  </li>
-					</div>
-			   </li>
-
-
-
-
-
-				{/* <li>
-					<Link to="/dashboard/vitsup" activestyle="true">
-						<img src={ta} alt="crop" />
-					</Link>
-					<p>Tech in Agriculture</p>
-				</li>
-				<li>
-					<Link to="/dashboard/marketplace" activestyle="true">
-						<img src={fn} alt="crop" />
-					</Link>
-					<p>Finest Tips</p>
-				</li> */}
-			</ul>
-		</div>
-	</div>
+  <div className="portal-content">
+    {/* <MinNav /> */}
+    <div className="portal-page-content">
+      <ul className="lec">
+        {courses.map((course, index) => (
+          <li key={index}>
+            <Link to={course.path} activestyle="true">
+              <img src={course.imgSrc} alt={course.imgAlt} />
+            </Link>
+            <div className='learnCard'>
+              <ul>
+                <h3>{course.title}</h3>
+                <p>{course.price}</p>
+              </ul>
+              <li className='courseDetails'>
+                <p>{course.description}</p>
+              </li>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
 );
 
 export default Learn;
